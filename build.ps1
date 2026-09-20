04b38bae8bdb13e9f1513ae1bbb91197a721891e
MVriens <markvriens@gmail.com>
2026-09-20 20:11:06 +0200

    builder file

--- build.ps1
+++ build.ps1
@@ -1,26 +1,26 @@
 $ErrorActionPreference = 'Stop'
-
+$moduleName = 'Build-JsonValidator'
 # Set up output directories and paths for the build artifacts
-
-$binReleaseDir = Join-Path $PSScriptRoot 'src/modules/Build-JsonValidator/bin/Release/net8.0'
-$binDll = Join-Path $outDir 'Build-JsonValidator.dll'
+$outDir = Join-Path $PSScriptRoot "release/$moduleName"
+$binReleaseDir = Join-Path $PSScriptRoot "src/modules/$moduleName/bin/Release/net8.0"
+$binDll = Join-Path $outDir "$moduleName.dll"
 
 New-Item -ItemType Directory -Force -Path $outDir | Out-Null
 
-$srcDir = Join-Path $PSScriptRoot 'src/modules/Build-JsonValidator'
-$srcProject = Join-Path $srcDir 'Build-JsonValidator.csproj'
+$srcDir = Join-Path $PSScriptRoot "src/modules/$moduleName"
+$srcProject = Join-Path $srcDir "$moduleName.csproj"
 dotnet build $srcProject -c Release
 
 Copy-Item `
-  (Join-Path $srcDir "Build-JsonValidator.psd1") `
-  (Join-Path $outDir "Build-JsonValidator.psd1") `
+  (Join-Path $srcDir "$moduleName.psd1") `
+  (Join-Path $outDir "$moduleName.psd1") `
   -Force
 
 Copy-Item `
-  (Join-Path $binReleaseDir 'Build-JsonValidator.dll') `
+  (Join-Path $binReleaseDir "$moduleName.dll") `
   $binDll `
   -Force
-
+# Copy dependencies from the build output to the release directory
 Copy-Item `
   (Join-Path $binReleaseDir 'Newtonsoft.Json.dll') `
   (Join-Path $outDir 'Newtonsoft.Json.dll') `
@@ -31,7 +31,7 @@ Copy-Item `
   (Join-Path $outDir 'Newtonsoft.Json.Schema.dll') `
   -Force
 
-Import-Module ./out/Build-JsonValidator.psd1 -Force
+Import-Module $outDir/$moduleName.psd1 -Force
 Get-JsonSchemaValidation -Json '{"name": "Mark"}' -Schema '{"type": "object", "properties": {"name": {"type": "string"}}}'
 
 Get-Module Build-JsonValidator | Remove-Module -Force
